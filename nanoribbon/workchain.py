92a1ec597a482c4c69ed004afd8215a58f7f4927
Ole Schuett <ole.schuett@empa.ch>
2017-08-22 18:11:12 +0200

    add orbital export

--- nanoribbon/workchain.py
+++ nanoribbon/workchain.py
@@ -1,4 +1,3 @@
-
 from aiida.orm.data.structure import StructureData
 from aiida.orm.data.parameter import ParameterData
 from aiida.orm.data.array.kpoints import KpointsData
@@ -30,9 +29,10 @@ class NanoribbonWorkChain(WorkChain):
             #cls.run_cell_opt1,
             #cls.run_cell_opt2,
             cls.run_scf,
-            cls.run_bands,
-            #cls.run_hartree_export,
+            cls.run_export_orbitals,
+            #cls.run_export_hartree,
             #cls.calc_vacuum_level,
+            #cls.run_bands,
         )
         spec.dynamic_output()
 
@@ -45,37 +45,94 @@ class NanoribbonWorkChain(WorkChain):
     def run_cell_opt2(self):
         prev_calc = self.ctx.cell_opt1
         assert(prev_calc.get_state() == 'FINISHED')
-        structure = prev_calc.out['output_structure']
+        structure = prev_calc.out.output_structure
         return self._submit_pw_calc(structure, label="cell_opt2", runtype='vc-relax', precision=1.0, min_kpoints=1)
 
     #============================================================================================================
     def run_scf(self):
         #prev_calc = self.ctx.cell_opt2
         #assert(prev_calc.get_state() == 'FINISHED')
-        #structure = prev_calc.out['output_structure']
+        #structure = prev_calc.out.output_structure
         structure = self.inputs.structure
         return self._submit_pw_calc(structure, label="scf", runtype='scf', precision=2.0, min_kpoints=5)
 
     #============================================================================================================
-    def run_bands(self):
+    def run_export_orbitals(self):
+        self.report("Running pp.x to export KS orbitals")
+
+        inputs = {}
+        inputs['code'] = self.inputs.pp_code
+
         prev_calc = self.ctx.scf
         assert(prev_calc.get_state() == 'FINISHED')
+        inputs['parent_folder'] = prev_calc.out.remote_folder
+
         structure = prev_calc.inp.structure
-        parent_folder = prev_calc.out['remote_folder']
-        return self._submit_pw_calc(structure, label="bands", parent_folder=parent_folder, runtype='bands', precision=4.0, min_kpoints=10)
+        cell_a = structure.cell[0][0]
+        cell_b = structure.cell[1][1]
+        cell_c = structure.cell[2][2]
+        midz = cell_c/2 + 1
+
+        nel = prev_calc.res.number_of_electrons
+        nkpt = prev_calc.res.number_of_k_points
+        kband1 = int(nel/2) - 1
+        kband2 = int(nel/2) + 2
+        kpoint1 = round(0.1*nkpt)
+        kpoint2 = round(nkpt+1-0.1*nkpt)
+
+        parameters = ParameterData(dict={
+                  'inputpp':{
+                      'plot_num': 7, # contribution of a selected wavefunction to charge density
+                      'kpoint(1)': kpoint1,
+                      'kpoint(2)': kpoint2,
+                      'kband(1)': kband1,
+                      'kband(2)': kband2,
+                  },
+                  'plot':{
+                      'iflag': 2, # 2D plot
+                      'output_format': 7, # format suitable for gnuplot   (2D) x, y, f(x,y)
+                      'x0(1)': 0.0, #3D vector, origin of the plane (in alat units)
+                      'x0(2)': 0.0,
+                      'x0(3)': midz/cell_a,
+                      'e1(1)': cell_a/cell_a, #3D vectors which determine the plotting plane (in alat units)
+                      'e1(2)': 0.0,
+                      'e1(3)': 0.0,
+                      'e2(1)': 0.0,
+                      'e2(2)': cell_b/cell_a,
+                      'e2(3)': 0.0,
+                      'nx': 10, # Number of points in the plane
+                      'ny': 10,
+                      'fileout': '_orbital_midz.dat',
+                  },
+        })
+        inputs['parameters'] = parameters
+
+ #       settings = ParameterData(dict={'additional_retrieve_list':['vacuum_hartree.dat']})
+ #       inputs['settings'] = settings
+
+        inputs['_options'] = {
+            "resources": {"num_machines": 1},
+            "max_wallclock_seconds": 10 * 60,
+            # workaround for bug in PpCalculator. We don't want to retrive this huge intermediate file.
+#            "append_text": u"rm -v aiida.filplot\n",
+        }
+
+        future = submit(PpCalculation.process(), **inputs)
+        return ToContext(hartree=Calc(future))
+
 
     #============================================================================================================
-    def run_hartree_export(self):
+    def run_export_hartree(self):
         self.report("Running pp.x to export hartree potential")
 
         inputs = {}
         inputs['code'] = self.inputs.pp_code
-        
+
         prev_calc = self.ctx.scf
         assert(prev_calc.get_state() == 'FINISHED')
-        inputs['parent_folder'] = prev_calc.out['remote_folder']
+        inputs['parent_folder'] = prev_calc.out.remote_folder
 
-        structure = prev_calc.out['output_structure']
+        structure = prev_calc.out.output_structure
         cell_a = structure.cell[0][0]
         cell_b = structure.cell[1][1]
         cell_c = structure.cell[2][2]
@@ -108,7 +165,7 @@ class NanoribbonWorkChain(WorkChain):
 
         inputs['_options'] = {
             "resources": {"num_machines": 1},
-            "max_wallclock_seconds": 2 * 60,
+            "max_wallclock_seconds": 10 * 60,
             # workaround for bug in PpCalculator. We don't want to retrive this huge intermediate file.
             "append_text": u"rm -v aiida.filplot\n",
         }
@@ -122,8 +179,8 @@ class NanoribbonWorkChain(WorkChain):
         self.report("Calculating vacuum level")
         prev_calc = self.ctx.hartree
         assert(prev_calc.get_state() == 'FINISHED')
-        
-        fn = prev_calc.out['retrieved'].get_abs_path("vacuum_hartree.dat")
+
+        fn = prev_calc.out.retrieved.get_abs_path("vacuum_hartree.dat")
         data = np.loadtxt(fn)
         vacuum_level = np.mean(data[:,2])
         self.report("Found vacuum level: %f"%vacuum_level)
@@ -131,6 +188,14 @@ class NanoribbonWorkChain(WorkChain):
         output_parameters = ParameterData(dict={'vacuum_level':vacuum_level})
         self.out("output_parameters", output_parameters)
 
+
+    #============================================================================================================
+    def run_bands(self):
+        prev_calc = self.ctx.scf
+        assert(prev_calc.get_state() == 'FINISHED')
+        structure = prev_calc.inp.structure
+        parent_folder = prev_calc.out.remote_folder
+        return self._submit_pw_calc(structure, label="bands", parent_folder=parent_folder, runtype='bands', precision=4.0, min_kpoints=10)
 
     #============================================================================================================
     def _submit_pw_calc(self, structure, label, runtype, precision, min_kpoints, parent_folder=None):
@@ -163,15 +228,15 @@ class NanoribbonWorkChain(WorkChain):
 
         if runtype == "bands":
             settings['also_bands'] = True # instruction for output parser
-        
+
         inputs['settings'] = ParameterData(dict=settings)
-        
+
 #         self.report("precision %f"%precision)
 #         self.report("nkpoints %d"%nkpoints)
 #         self.report("npools %d"%npools)
 #         self.report("natoms %d"%natoms)
 #         self.report("nnodes %d"%nnodes)
-                
+
         future = submit(PwCalculation.process(), **inputs)
         return ToContext(**{label:Calc(future)})
 
@@ -196,7 +261,7 @@ class NanoribbonWorkChain(WorkChain):
                      'scf_must_converge': False,
                      },
         }
-        
+
         if runtype == "vc-relax":
             params['CELL'] = {'cell_dofree': 'x'} # in y and z direction there is only vacuum
 
@@ -250,3 +315,5 @@ class NanoribbonWorkChain(WorkChain):
             else:
                 start_mag[i.name] = 0.0
         return start_mag
+
+#EOF
